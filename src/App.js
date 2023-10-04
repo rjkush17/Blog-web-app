72e6a10b9bbe4969255effa41459c59ecea565ba
rjkush17 <rishabhkushwahrj@gmail.com>
2023-10-04 11:03:50 +0530

    Bloglist component added with fake data and made use params function too

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import Navbar from "./Components/Navbar";
 import Hero from './Components/Hero';
+import {  Route, Routes } from "react-router-dom";
+import BlogsList from "./Components/BlogsList";
+import Blog from "./Blog Pages/Blog";
+import NotFoundPage from "./Components/NotFoundPage";
 
 
 function App() {
   return (
     <div className="font-nunito ">
+    
+     
      <Navbar/>
-     <Hero/>
+     <Routes>
+      <Route path='/' element={<Hero/>}/>
+      <Route path='/blog/:id' element={<Blog/>}/>
+      <Route path='/*' element={<NotFoundPage/>}/>
+     </Routes>
+     {/* <BlogsList/> */}
     </div>
+    
   );
 }
 
---
+++ src/Components/BlogsComponents.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+function BlogsComponents({blog}) {
+  return (
+    <div className='border-2 border-gray-300 flex-1'>
+        <img className='h-[250px] w-full object-cover' src="https://www.irdes.fr/imgs2017/images/about-imgs.jpg" alt="Blog IMG" />
+        <div className='p-2'>
+            <h1 className='text-primary font-bold'>{blog.title}</h1>
+            <p className='bg-primary w-fit text-sm font-semibold py-1 px-4 text-white rounded-full mt-3 text-bold'>{blog.category}</p>
+            <p className='text-sm my-3'>{blog.description}</p>
+            <NavLink to={`/Blog/:${blog.id}`} className='bg-primary rounded p-1 text-white mt-auto'>...See Blog</NavLink>
+    
+        </div>
+    </div>
+  )
+}
+
+export default BlogsComponents
